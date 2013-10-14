65205d09912b30b89a1f36756aad8aa6543ac3f0
tristantao <tao.tristan@gmail.com>
2013-10-13 17:56:49 -0700

    getting close to being done

--- parse.py
+++ parse.py
@@ -5,13 +5,61 @@ text_file = open("data/training/ipod.txt", "r")
 kill = 0
 new_feature_flag = False
 
-for line in text_file:
-    if re.search("[\+|\-][0-9]+", line):
-        print line
-    if kill >= 10:
+training_data = {} #<sentence:sent_num>, i.e. <"I love it!":3>
+
+temp_features = []
+new_sent_val = False
+
+def add_data(training, temp_feat):
+    one = temp_feat[0]
+    one = one.replace("}", "]")
+    try:
+        val = int(re.search('\[(.*?)\]', one).group(1))
+    except TypeError as tE:
+        print "Couldn't parse sentiment value"
+    except IndexError as iE:
+        print "Index error"
+        print one
+        print re.findall('\[(.*)\]', one)
+    except ValueError as vE:
+        print "Value Error"
+        re.match('\[(.*)\]', one)
+        print re.search('\[(.*)\]', one).groups()
+    except AttributeError as aE:
+        print "Attribute Error"
+        print one
+        print training_data
         sys.exit()
+    for index in xrange(0, len(temp_feat)):
+        training[temp_feat[index]] = val
+
+
+for line in text_file:
+    print line
+    if (len(re.findall(r"\[[\+|\-][0-9]\]+", line)) >= 1) and not new_sent_val: #new one
+        temp_features.append(line)
+        new_sent_val = True
+        continue
+    elif (len(re.findall(r"\[[\+|\-][0-9]\]+", line)) >= 1) and new_sent_val: #end
+        add_data(training_data, temp_features) #Do the work,
+        temp_features = []
+        temp_features.append(line)
+        new_sent_val = True
+        continue
+
+    if new_sent_val:
+        temp_features.append(line)
+    if kill >= 10000:
+        break
     kill += 1
 
-lines = text_file.readlines()
+add_data(training_data, temp_features) #last one, corner case
 
-print lines
+print len(training_data)
+print training_data
+print debug_list
+a = ["features[-2]## There isn't much features on the iPod at all, except games"]
+add_data({}, a)
+
+
+
