ec90212efe560192f4a3ea8f10f2c8971d9f1c6a
tristantao <tao.tristan@gmail.com>
2013-10-15 22:15:14 -0700

    removing hash ## in parse.py

--- parse.py
+++ parse.py
@@ -86,7 +86,7 @@ def read_test_data(path):
     temp_dict = {}
     for line in text_file:
         sep = line.split("\t")
-        temp_dict[sep[0]] = sep[1].strip()
+        temp_dict[sep[0]] = sep[1].strip().replace("##", "")
     #print temp_dict
     return temp_dict
 
